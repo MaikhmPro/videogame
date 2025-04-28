155f89efd5fac84548ed2effbf74d5d8f4a6aabf
MaikhmPro <mehueso@ucundinamarca.edu.co>
2025-04-28 12:30:01 -0500

    Videogame con movimientos

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
 import pygame 
+from personaje import Personaje
+import contantes
+
+player = Personaje(50,50)
 pygame.init()
 
-ancho = 800
-alto = 600
+ancho = contantes.ANCHO_VENTANA
+alto = contantes.ALTO_VENTANA
 ventana = pygame.display.set_mode((ancho,alto))
 
+pygame.display.set_caption("Mi first videogame")
+
+"""Variables de mvimiento"""
+mover_arriba = False
+mover_abajo = False
+mover_izquierda = False
+mover_derecha = False
+
 run = True
+"""Calcular movimiento"""
 while run :
+
+    delta_x = 0
+    delta_y = 0
+    if mover_derecha == True:
+        delta_x= 5
+    if mover_izquierda == True:
+        delta_x= -5
+    if mover_arriba == True:
+        delta_y= -5 
+    if mover_abajo == True:
+        delta_Y= 5
+    player.movimiento(delta_x, delta_y)
+    player.dibujar(ventana)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                mover_izquierda = True
+                if event.key == pygame.K_d:
+                    mover_derecha = True
+                    if event.key == pygame.K_w:
+                        mover_arriba = True
+                        if event.key == pygame.K_s:
+                            mover_abajo = True
 
+
+pygame.display.update()
 pygame.quit()
--- personaje.py
+++ personaje.py
@@ -1,11 +1,18 @@
 import pygame 
-
+import contantes
 class Personaje():
     def __init__(self,x,y):
-        self.forma= pygame.rect(0,0,20,20)
+        self.forma= pygame.Rect(0,0,contantes.ANCHO_PERSONAJE,contantes.ALTO_PERSONAJE)
         self.forma.center=(x,y)
     def dibujar(self,ventana):
-        pygame.draw.rect(ventana, (255,255,0, self.forma))
+         self.forma = pygame.Rect(0, 0, contantes.ANCHO_PERSONAJE, contantes.ALTO_PERSONAJE)
+    def movimiento(self,delta_x,delta_y):
+        self.forma.x = self.forma.x + delta_x
+        self.forma.y = self.forma.y + delta_y
+
+
+
+
 
 
         
